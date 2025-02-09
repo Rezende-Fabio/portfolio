5ee452e46f472b2e464c0a7f7c7b88541742d5a5
Rezende-Fabio <rezendefabio337@gmail.com>
2025-02-09 13:54:39 -0300

    FEAT: added section about me

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,37 @@
-import { Component, ElementRef, Renderer2 } from '@angular/core';
+import { Component, AfterViewInit, HostListener, ElementRef, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './sections/header/header.component';
 import { StartComponent } from './sections/start/start.component';
+import { AboutMeComponent } from './sections/about-me/about-me.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, StartComponent],
+  imports: [HeaderComponent, StartComponent, AboutMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  
+  constructor(private el: ElementRef, private renderer: Renderer2) { }
+
+  ngAfterViewInit() {
+    this.animeScroll(); // Chama a função no início
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll() {
+    this.animeScroll();
+  }
+
+  private animeScroll() {
+    const windowTop = window.scrollY + window.innerHeight * 0.85;
+    const elements = this.el.nativeElement.querySelectorAll('[data-anime]');
+
+    elements.forEach((element: HTMLElement) => {
+      if (windowTop > element.offsetTop) {
+        this.renderer.addClass(element, 'animate');
+      } else {
+        this.renderer.removeClass(element, 'animate');
+      }
+    });
+  }
 }
