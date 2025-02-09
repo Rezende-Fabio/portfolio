baf8a9774b8e1194c5fa44cbb7791b7fad9ca72d
Rezende-Fabio <rezendefabio337@gmail.com>
2025-02-08 23:06:18 -0300

    FEAT: added sections header and start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './sections/header/header.component';
+import { StartComponent } from './sections/start/start.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, StartComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'portfolio';
+  
 }
